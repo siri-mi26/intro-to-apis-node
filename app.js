a48d4f544e479fea658013518178538fdf57479b
Glitch (clammy-coral-pirate) <none>
2023-01-15 04:01:34 +0000

    Updated with Glitch - added error-catching if fails

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const TwilioClient = require("twilio");
 const app = express();
 const port = 3000;
 
-
 const client = new TwilioClient();
 const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
 
@@ -15,10 +14,9 @@ app.use(express.static("public"));
 app.use(bodyParser.json());
 
 app.get("/api/compliments", async (req, res) => {
-  
-  const sentMessages = await client.messages.list({from:twilioPhoneNumber});
-  
-  const compliments = sentMessages.map(message => message.body);
+  const sentMessages = await client.messages.list({ from: twilioPhoneNumber });
+
+  const compliments = sentMessages.map((message) => message.body);
   res.json(compliments);
 });
 
@@ -26,8 +24,13 @@ app.post("/api/compliments", async (req, res) => {
   const to = req.body.to;
   const from = process.env.TWILIO_PHONE_NUMBER;
   const body = `${req.body.sender} says: ${req.body.receiver} is ${req.body.compliment}. See more compliments at ${req.headers.referer}`;
-  // TODO: Send a message
-  await client.messages.create({to, from, body});
+
+  try {
+    await client.messages.create({ to, from, body });
+  } catch (err) {
+    res.status(err.status).json({ success: false, message: err.message });
+  }
+
   res.json({ success: true });
 });
 
